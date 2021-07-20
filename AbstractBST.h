49d8954e3814f64bf7299cb41af2960e6289365e
nischalshakya777 <69291726+nischalshakya777@users.noreply.github.com>
2021-07-20 22:09:49 +0545

    Update AbstractBST.h

--- AbstractBST.h
+++ AbstractBST.h
@@ -4,7 +4,7 @@
 class AbstractBST
 {
     public:
-    virtual bool isEmpty() = 0;
+    virtual bool isEmpty()=0;
     virtual void addBST(int value) = 0;
     virtual bool searchBST(int targetkey) = 0;
     virtual void removeBST(int key) = 0;
