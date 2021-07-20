f1db17ae449de09290e89db93b893f9e6e508612
nischalshakya777 <69291726+nischalshakya777@users.noreply.github.com>
2021-07-20 19:19:22 +0545

    Update AbstractBST.h

--- AbstractBST.h
+++ AbstractBST.h
@@ -4,11 +4,7 @@
 class AbstractBST
 {
     public:
-    virtual bool isEmpty()=0;
-    virtual void add(int key, int value) = 0;
-    virtual void max(int &output) = 0;
-    virtual void min(int &output) = 0;
-    virtual bool exists(int &targetkey) = 0;
-    virtual void inorder() = 0;
+    virtual void addBST(int value) = 0;
+    virtual bool searchBST(int &targetkey) = 0;
+    virtual void removeBST(int key, int value) = 0;
 };
-
