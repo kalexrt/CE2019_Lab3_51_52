c8cc2320d4e1a4d618f8d1451500b489e09d55f8
nischalshakya777 <69291726+nischalshakya777@users.noreply.github.com>
2021-07-22 09:27:07 +0545

    Add files via upload

---
+++ ArrayBST.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "AbstractBST.h"
+#define max_size 64
+
+struct ArrayNode 
+{
+    int key;
+    int value;
+    
+    ArrayNode(int key, int value) : key(key), value(value) {}
+};
+class ArrayBST : public AbstractBST
+{
+    private:
+        ArrayNode* nodes[max_size]; 
+
+    public:
+        ArrayBST();
+        ~ArrayBST();
+        bool isEmpty();
+        void add(int value);
+        void remove(int key);
+        bool search(int targetkey);     
+   
+};
